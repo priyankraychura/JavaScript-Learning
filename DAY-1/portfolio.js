212ea3cf68809ea819cc6cd173378adeb096d6d2
priyankraychura <priyankraychura@gmail.com>
2024-07-29 21:59:08 +0530

    DAY-1 Updated

---
+++ DAY-1/portfolio.js
@@ -0,0 +1,24 @@
+document.getElementById("data").innerHTML = `
+<h1> Priyank Raychura </h1>
+<h2> Full-Stack Developer </h2>
+<h2> About me:  </h2>
+    <p> Technologically adept enthusiast with a solid foundation in 
+    Computer Applications, eager to apply my robust knowledge in 
+    technology, continuous learning mindset, and collaborative spirit 
+    to contribute effectively to global innovation. </p>
+<h3> Languages Known:  </h3>
+<ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Bootstrap</li>
+    <li>MediaQuery</li>
+    <li>JQuery</li>
+</ul>
+
+<h3> Contact me: </h3>
+<ul>
+    <li>Address: 8, Rajputpara, RLBH Rajkot,  360001</li>
+    <li>Phone: 6359467208</li>
+    <li>Email: priyankraychura@gmail.com</li>
+</ul>
+`
