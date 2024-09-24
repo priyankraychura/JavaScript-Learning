b3f470a0d978366aca082a2466e38ccd8f693833
priyankraychura <priyankraychura@gmail.com>
2024-09-24 17:53:37 +0530

    form completed

--- DAY-17/FormValidation/script.js
+++ DAY-17/FormValidation/script.js
@@ -1,7 +1,39 @@
+const signupDiv = document.getElementById("signupDiv");
+const loginDiv = document.getElementById("loginDiv");
+const signupForm = document.getElementById("signupForm");
+const loginForm = document.getElementById("loginForm");
 const users = [];
 
-let form = document.getElementById("signupForm");
-form.addEventListener("submit", (e) => {
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let email = document.getElementById("user-email").value;
+    let pass = document.getElementById("user-pass").value;
+
+    let result = users.filter((user) => {
+        return user.email == email && user.password == pass;
+    });
+
+    if(result.length){
+        Swal.fire({
+            title: "Loggin Successful!",
+            text: `Welcome, ${result[0].username}`,
+            icon: "success"
+        });
+    } else{
+        Swal.fire({
+            icon: "error",
+            title: "Login Failed!",
+            text: "Incorrect Usrname or Password",
+            footer: '<a href="#">Forgot Password?</a>'
+        });
+    }
+
+    
+    loginForm.reset();
+})
+
+signupForm.addEventListener("submit", (e) => {
     e.preventDefault();
     
     let username = document.querySelector("#username").value;
@@ -16,6 +48,24 @@ form.addEventListener("submit", (e) => {
 
     users.push(obj);
     console.log(users)
-    console.log("abc")
+
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "You're registered successfully!",
+        showConfirmButton: false,
+        timer: 1500
+    });
+
+    signupForm.reset();
+});
+
+document.getElementById("goToLogin").addEventListener("click", () => {
+    signupDiv.style.display = "none";
+    loginDiv.style.display = "block";
+});
+document.getElementById("goToRegister").addEventListener("click", () => {
+    loginDiv.style.display = "none";
+    signupDiv.style.display = "block";
 });
 
