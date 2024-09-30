3402fbf206ff650aaa56a67b525a418c546177f3
priyankraychura <priyankraychura@gmail.com>
2024-09-30 16:12:18 +0530

    Errors resolved

--- DAY-19/script.js
+++ DAY-19/script.js
@@ -19,27 +19,26 @@ class Account{
 }
 
 class ATM extends Account{
+    constructor(accNumber, accHolder, bal){
+        super(accNumber, accHolder, bal);
+    }
     displayOptions(){
-        dispOptions = `
-            1. Check Balance
-            2. Deposite
-            3. Withdraw
-        `;
+        const dispOptions = "1. Check Balance \n2. Deposite \n3. Withdraw";
         console.log(dispOptions);
     }
 
-    performOperation(operation, amount){
+    performOperation(operation, amount = 0){
         switch(operation){
             case 1:
-                console.log(this.checkBalance());
+                console.log("Your current balance is: ", this.checkBalance());
                 break;
             case 2:
                 this.deposit(amount);
-                console.log("Your updated balance is: ", this.checkBalance());
+                console.log("Balance after deposite is: ", this.checkBalance());
                 break;
             case 3:
                 this.withdraw(amount);
-                console.log("Your updated balance is: ", this.checkBalance());
+                console.log("Balance after withdrawal is: ", this.checkBalance());
                 break;
             default:
                 console.log("Please enter a valid choice");
@@ -47,8 +46,10 @@ class ATM extends Account{
     }
 }
 
-const acc = new Account(12345678910, "Priyank", 1000);
-console.log(acc.checkBalance())
+// const acc = new Account(12345678910, "Priyank", 1000);
 
-const transn = new ATM();
+const transn = new ATM(12345678910, "Priyank", 1000);
 transn.displayOptions();
+transn.performOperation(1);
+transn.performOperation(2, 500);
+transn.performOperation(3, 100);
